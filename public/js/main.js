2e5703c8939d1cb07a4715ae4829b88045c4b709
unknown <mohammedhussein1225m@gmail.com>
2025-02-13 10:49:41 +0200

    Edite navbar for mobile

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,24 @@
-const link = document.querySelectorAll(".links .link");
+//  navbar script for mobile
+function toggle_navbar() {
+  let links = document.querySelector(".nav-mobile .head .links");
 
-for (let i = 0; i < link.length; i++) {
-  link[i].addEventListener("click", function () {
-    for (let j = 0; j < link.length; j++) {
-      link[j].classList.remove("animation");
+  // click on bars to open nvbar
+  let icon = document.querySelector(".nav-mobile .toggle");
+  icon.addEventListener("click", function () {
+    links.classList.remove("-translate-x-full");
+  });
+
+  // click on btn close to close nvbar
+  let close = document.querySelector(".nav-mobile .head .close");
+  close.addEventListener("click", function () {
+    links.classList.add("-translate-x-full");
+  });
+
+  // click on anywher on window  to close nvbar
+  window.addEventListener("click", (event) => {
+    if (!links.contains(event.target) && !icon.contains(event.target)) {
+      links.classList.add("-translate-x-full");
     }
-    link[i].classList.add("animation");
   });
 }
+toggle_navbar();
